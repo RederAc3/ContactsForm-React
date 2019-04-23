5800e1fdce402860d8e961a1d4c9a2cd14b01beb
Andrzej Kot <reder.ac3@gmail.com>
2019-04-23 23:23:11 +0200

    Add project

---
+++ components/Contacts.js
@@ -0,0 +1,16 @@
+var Contacts = React.createClass({
+    propTypes: {
+        items: React.PropTypes.array.isRequired,
+    },
+    render: function() {
+        var contacts = this.props.items.map(function(contact) {
+            return React.createElement(Contact, {item:contact, key: contact.id})
+        });
+
+        // console.log(this.props.items);
+
+        return(
+            React.createElement('ul', {className: 'conractsList'}, contacts)
+        );
+    }
+});
